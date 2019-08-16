6330bb3fbe121415200a00379c9ee471e06aea05
ChaoYIN <yinchao428@hotmail.com>
2019-08-16 22:04:24 +1000

    Updates

---
+++ precache-manifest.2cf2e1b42336891473921aa6ce6be301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596bc342a805ad69af73ac42380c7bf2",
+    "url": "/github-user-finder/index.html"
+  },
+  {
+    "revision": "927857cb5b28a986ce7c",
+    "url": "/github-user-finder/static/css/main.dc943fc2.chunk.css"
+  },
+  {
+    "revision": "7e635591e77e0e4a2da9",
+    "url": "/github-user-finder/static/js/2.6c809506.chunk.js"
+  },
+  {
+    "revision": "927857cb5b28a986ce7c",
+    "url": "/github-user-finder/static/js/main.076d47b3.chunk.js"
+  },
+  {
+    "revision": "ba53de60ca2bce281fd0",
+    "url": "/github-user-finder/static/js/runtime~main.3387d241.js"
+  }
+]);
